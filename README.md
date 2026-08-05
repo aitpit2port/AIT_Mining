e140b35e0787ba42190eb710eae32fd361e5d7c5
aitpit2port <ait.pit2port@info-ait.com>
2026-08-05 11:20:44 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# AIT GeoMine 360 V3.2
+
+Secure GitHub Pages frontend connected to Google Apps Script, Google Sheets and Google Drive.
---
+++ dashboard/backend-config.js
@@ -0,0 +1,7 @@
+window.AIT_BACKEND_CONFIG = {
+  webAppUrl: 'PASTE_APPS_SCRIPT_EXEC_URL_HERE',
+  githubOrigin: window.location.origin,
+  loginWidth: 480,
+  loginHeight: 640,
+  tokenStorageKey: 'ait_geomine_session_v2'
+};
